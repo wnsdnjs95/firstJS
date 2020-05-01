a812f7927424e8456d9f2030c355d65d1b4577df
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-05-01 19:25:20 +0900

    function2

--- function.js
+++ function.js
@@ -1,5 +1,33 @@
-function sayHello(name, age) {
-  console.log("Hello!", name, " you have", age, "years of age.");
-}
+// function sayHello(name, age) {
+//   return `Hello ${name} you are ${age} years old`;
+// }
 
-sayHello("Junwon", 26);
+// console.log(greetJunwon);
+// const greetJunwon = sayHello("Junwon", 26);
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  multify: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  power: function (a, b) {
+    return a ** b;
+  },
+};
+
+const calculatorResult = {
+  plus: calculator.plus(2, 2),
+  minus: calculator.minus(2, 2),
+  multify: calculator.multify(2, 2),
+  divide: calculator.divide(2, 2),
+  power: calculator.power(3, 3),
+};
+console.log(calculatorResult);
