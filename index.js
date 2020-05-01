2b9cdf025e305cb25951245034ac4b9e3e2b50ec
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-05-01 13:55:54 +0900

    js data type
    string, number, boolean, float

---
+++ index.js
@@ -0,0 +1,22 @@
+// 한줄 주석처리
+/*
+주석처리
+*/
+var a = 221;
+var b = a - 5;
+a = 4;
+
+console.log(b, a);
+
+/* String
+const what = "Junwon";
+
+Boolean
+const wat = true;
+
+Number
+const watt = 666;
+
+Float
+const wat = 55.1;
+*/
