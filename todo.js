151730942a49c21eb8bc26aa852c09b1d29e1780
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-05-07 14:02:58 +0900

    ToDo 지우기 버튼

--- todo.js
+++ todo.js
@@ -4,7 +4,18 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,12 +25,13 @@ function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
-  const newID = toDos.length + 1;
-  delBtn.innerText = "✖️";
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
-  li.id = newId;
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
   const toDoObj = {
     text: text,
@@ -37,9 +49,9 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const loadedtoDos = localStorage.getItem(TODOS_LS);
+  const loadedToDos = localStorage.getItem(TODOS_LS);
   if (toDos !== null) {
-    const parsedToDos = JSON.parse(loadedtoDos);
+    const parsedToDos = JSON.parse(loadedToDos);
     parsedToDos.forEach(function (toDo) {
       paintToDo(toDo.text);
     });
